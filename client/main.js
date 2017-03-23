7a8c111939915fa9e47ad913f572f3f126fbedad
Gerlis Alvarez Cardona <Galvarez@intergrupo.com>
2017-03-23 11:06:34 -0500

    Creacion del chat

---
+++ client/main.js
@@ -0,0 +1,19 @@
+﻿var socket = io.connect('http://10.75.11.132:6677', { 'forceNew': true });
+
+socket.on('messages', function (data) {
+    console.log(data);
+    render(data);
+});
+
+function render(data) {
+    var html = data.map(function (message, index) {
+        return (`
+            <div class ="message">
+                <strong>${message.nickname}</strong> Dice:
+            <p>${message.text}</p>
+            </div>
+         `);
+    }).join(' ');
+
+    document.getElementById('messages').innerHTML = html;
+}
---
+++ server/index.js
@@ -0,0 +1,26 @@
+﻿var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+app.use(express.static('client'));
+
+app.get('/hola-mundo', function (req, res) {
+    res.status(200).send('Hola mundo');
+});
+
+var messages = [{
+    id: 1,
+    text: 'Bienvenido al chat Papus!!',
+    nickname: 'Bot'
+}];
+io.on('connection', function (socket) {
+    console.log("el nodo con IP: " + socket.handshake.address + " se ha conectado");
+
+    socket.emit('messages', messages);
+});
+
+server.listen(6677, function () {
+    console.log('El servidor está funcionando');
+});
